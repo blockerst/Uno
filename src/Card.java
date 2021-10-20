1f13b854c0df6323a7978c51dd1c3b19cfc29b35
Sarpyildir <Sarpyildir@gmail.com>
2021-10-20 21:10:51 +0300

    getters and setters are added

--- src/Card.java
+++ src/Card.java
@@ -6,6 +6,12 @@ public class Card {
         value = val;
         color = c;
     }
+    // getters
+    public int getValue(){ return value; }
+    public int getColor(){ return color; }
+    //setters
+    public void setValue(int value){this.value = value; }
+    public void setColor(int color){this.color = color; }
 
     public String toString(){
         String out = "";
@@ -23,21 +29,11 @@ public class Card {
             out = "Red ";
         }
 
-        if( value < 10){
-            out += "" + value;
-        }
-        else if( value == 11){
-            out += "+2";
-        }
-        else if( value == 12){
-            out += "+4";
-        }
-        else if( value == 13){
-            out += "Skip";
-        }
-        else if( value == 14 ){
-            out += "Wild";
-        }
+        if( value < 10 ){ out += "" + value; }
+        else if( value == 10 ){ out += "+2"; }
+        else if( value == 11 ){ out += "+4"; }
+        else if( value == 12 ){ out += "Skip"; }
+        else if( value == 13 ){out += "Wild"; }
 
         return out;
     }
