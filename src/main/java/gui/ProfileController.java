f73780e94d3475a1b1703424bfea8bac9b249cfc
Oğuz Akın <oguz.akin@ug.bilkent.edu.tr>
2021-12-18 19:54:34 +0300

    card image paths are modified

--- src/main/java/gui/ProfileController.java
+++ src/main/java/gui/ProfileController.java
@@ -79,7 +79,7 @@ public class ProfileController implements Initializable{
             ImageView iw = new ImageView();
             iw.setFitHeight(108);
             iw.setFitWidth(78);
-            iw.setImage(new Image("file:C:\\Users\\Asus\\Desktop\\UNO\\src\\main\\resources\\cards\\"+ rewards.get(i).getName()+".png"));
+            iw.setImage(new Image("file:src\\main\\resources\\cards\\"+ rewards.get(i).getName()+".png"));
             iw.setLayoutX(x);
             anchorpane.getChildren().add(iw);
             x += 88;
